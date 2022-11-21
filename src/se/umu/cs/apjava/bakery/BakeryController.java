bdf6707a4fcd27deafbc3a75be17e8e53c5a60e8
Emmy Lindgren <emmy.lindgren@hotmail.com>
2022-11-21 15:50:45 +0100

    Updated GUI to match new decorations

--- src/se/umu/cs/apjava/bakery/BakeryController.java
+++ src/se/umu/cs/apjava/bakery/BakeryController.java
@@ -1,22 +1,41 @@
 package se.umu.cs.apjava.bakery;
 
+import se.umu.cs.emli.ExtraLargeCake;
+import se.umu.cs.emli.SprinkledCake;
+import se.umu.cs.emli.TextedCake;
+
 public class BakeryController {
     private Order currentOrder=new Order();
     private Cake currentCake;
 
     String printOrder() {
-        finishOrder();
         StringBuilder stringBuilder=new StringBuilder();
         currentOrder.printOrder(stringBuilder);
         currentOrder=new Order();
         return stringBuilder.toString();
     }
 
-    private void finishOrder() {
+    public void finishOrder() {
         if (currentCake!=null) {
             currentOrder.addCake(currentCake);
         }
-        currentCake=null;
+        currentCake = null;
+    }
+
+    public void clearOrder(){
+        currentCake = null;
+    }
+
+    public void addSprinkles(){
+        currentCake = new SprinkledCake(currentCake);
+    }
+
+    public void extraLarge(){
+        currentCake = new ExtraLargeCake(currentCake);
+    }
+
+    public void addText(String text){
+        currentCake = new TextedCake(currentCake,text);
     }
 
     public void newCake(Cake cake) {
--- src/se/umu/cs/apjava/bakery/BakeryGuiBuilder.java
+++ src/se/umu/cs/apjava/bakery/BakeryGuiBuilder.java
@@ -1,35 +1,112 @@
 package se.umu.cs.apjava.bakery;
 
-import se.umu.cs.emli.ExtraLargeCake;
-import se.umu.cs.emli.SprinkledCake;
 import se.umu.cs.emli.StrawberryCake;
-import se.umu.cs.emli.TextedCake;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class BakeryGuiBuilder {
+    BakeryController bakeryController;
+    JTextArea textArea;
+    JPanel decoratorPanel;
+    JPanel cakePanel;
+
+    JCheckBox extraLargeCheckBox;
+    JCheckBox sprinkledCheckBox;
+    JTextField textDecoration;
+
     void buildGui() {
-        BakeryController bakeryController =new BakeryController();
+        bakeryController =new BakeryController();
         var frame=new JFrame();
-        frame.setSize(400,400);
-        var cakePanel=new JPanel();
-        JTextArea textArea = new JTextArea();
+        frame.setMinimumSize(new Dimension(500, 500));
+
+        textArea = new JTextArea();
         frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        var cakePanel = new JPanel();
+        cakePanel.setBorder(new CompoundBorder(BorderFactory.createTitledBorder("Order cakes")
+                ,new EmptyBorder(5,5,5,5)));
+        cakePanel.setLayout(new BoxLayout(cakePanel,BoxLayout.Y_AXIS));
+        cakePanel.add(makeCakePanel());
+        cakePanel.add(makeDecorationPanel());
+        cakePanel.add(makeOrderPanel());
+
+        frame.add(cakePanel,BorderLayout.NORTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private JPanel makeCakePanel(){
+
+        cakePanel=new JPanel();
+        cakePanel.setBorder(new CompoundBorder(BorderFactory.createTitledBorder("Choose type of cake")
+                ,new EmptyBorder(5,5,5,5)));
 
         var chocolateCakeButton=new JButton("Chocolate Cake");
         chocolateCakeButton.addActionListener(e->{
-            bakeryController.newCake(new TextedCake(new SprinkledCake(new ExtraLargeCake(new ChocolateCake())),"hej"));
+            bakeryController.newCake(new ChocolateCake());
+            cakeBuilderActive(true);
+            chocolateCakeButton.setEnabled(true);
         });
 
         var vanillaCakeButton=new JButton("Vanilla Cake");
         vanillaCakeButton.addActionListener(e->{
             bakeryController.newCake(new VanillaCake());
+            cakeBuilderActive(true);
+            vanillaCakeButton.setEnabled(true);
         });
 
         var strawberryCakeButton=new JButton("Strawberry Cake");
         strawberryCakeButton.addActionListener(e->{
             bakeryController.newCake(new StrawberryCake());
+            cakeBuilderActive(true);
+            strawberryCakeButton.setEnabled(true);
+        });
+
+        cakePanel.add(vanillaCakeButton);
+        cakePanel.add(chocolateCakeButton);
+        cakePanel.add(strawberryCakeButton);
+
+        return cakePanel;
+    }
+
+    private JPanel makeDecorationPanel(){
+        decoratorPanel = new JPanel();
+        decoratorPanel.setBorder(new CompoundBorder(BorderFactory.createTitledBorder("Add extras")
+                ,new EmptyBorder(5,5,5,5)));
+
+        extraLargeCheckBox = new JCheckBox("Extra large");
+        sprinkledCheckBox = new JCheckBox("With sprinkles");
+        textDecoration = new JTextField();
+
+        decoratorPanel.add(extraLargeCheckBox);
+        decoratorPanel.add(sprinkledCheckBox);
+        decoratorPanel.add(textDecoration,BorderLayout.LINE_END);
+
+        setDecoratorPanelEnable(false);
+
+        return decoratorPanel;
+    }
+
+    private JPanel makeOrderPanel(){
+        JPanel orderPanel = new JPanel();
+
+        var clearCurrentCakeButton =new JButton("Clear current cake");
+        clearCurrentCakeButton.addActionListener(e->{
+            bakeryController.clearOrder();
+            cakeBuilderActive(false);
+        });
+
+        var addToOrderButton=new JButton("Add cake to order");
+        addToOrderButton.addActionListener(e -> {
+            if(extraLargeCheckBox.isSelected()) bakeryController.extraLarge();
+            if(sprinkledCheckBox.isSelected()) bakeryController.addSprinkles();
+            if(!textDecoration.getText().isEmpty()) bakeryController.addText(textDecoration.getText());
+
+            bakeryController.finishOrder();
+            cakeBuilderActive(false);
         });
 
         var printButton=new JButton("Print Order");
@@ -38,13 +115,30 @@ public class BakeryGuiBuilder {
             textArea.append(orderString);
         });
 
-        cakePanel.add(vanillaCakeButton);
-        cakePanel.add(chocolateCakeButton);
-        cakePanel.add(strawberryCakeButton);
-        cakePanel.add(printButton);
+        orderPanel.add(clearCurrentCakeButton);
+        orderPanel.add(addToOrderButton);
+        orderPanel.add(printButton);
 
-        frame.add(cakePanel,BorderLayout.NORTH);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        return orderPanel;
     }
+
+    private void cakeBuilderActive(boolean bool){
+        setDecoratorPanelEnable(bool);
+        setCakePanelEnable(!bool);
+    }
+    
+    private void setDecoratorPanelEnable(boolean bool){
+        for (Component component: decoratorPanel.getComponents()) {
+            component.setEnabled(bool);
+            if(component instanceof JCheckBox box) box.setSelected(false);
+        }
+        decoratorPanel.setEnabled(bool);
+    }
+
+    private void setCakePanelEnable(boolean bool){
+        for (Component component: cakePanel.getComponents()) {
+            component.setEnabled(bool);
+        }
+    }
+
 }
